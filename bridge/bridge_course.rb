fafdfbcd3149d9574d5ad9a1d1ffce699670e3e4
Ivan Garcia <igtech03@gmail.com>
2018-02-26 14:48:40 -0700

    Update bridge course creation script
    Added parallel requests for creating courses in Bridge
    as well as using course templates more effectively when making requests

--- bridge/bridge_course.rb
+++ bridge/bridge_course.rb
@@ -1,33 +1,55 @@
 require 'typhoeus'
-require 'json'
-require 'time'
+require 'forgery'
 require '../config/config.rb'
 require '../rubyize_json'
 
 @base_url = '<bride_base_url>'
 @token = auth(1,'bridge')
 
-courses = <num_courses_to_create>
-
-course_counter = 1
-courses.times do
-  course_tmp={
-    'title'=> "Course #{course_counter}",
-    'is_published' => true,
-    'has_unpublished_changes' => false,
-    'default_days_until_due' => 30,
-    'course_type' => 'bridge'
-  }
-
-  course_resp = Typhoeus::Request.new(
-      @base_url + '/api/author/course_templates',
-      method: :post,
-      params: {
-        course_templates: { 'course' => course_tmp }
-      },
-      headers: { authorization: @token }
-  )
-  puts rubyize_json(course_resp)
-
-  course_counter += 1
+def courses(course_count)
+  if course_count >= 1
+    course_temp = []
+    course_count.times do
+      course_title = Forgery::LoremIpsum.words(rand(1..3), random:true)
+      course_description = Forgery::LoremIpsum.paragraphs(2, random: true)
+      course_temp << {
+          title: course_title,
+          description: course_description,
+          is_published: true,
+          has_unpublished_changes: false,
+          default_days_until_due: 10,
+          course_type: 'bridge'
+      }
+    end
+    create_courses(course_temp)
+  else
+    puts "Course count must be at least 1"
+  end
 end
+
+def create_courses(course_temp)
+  hydra = Typhoeus::Hydra.new(max_concurrency: 10)
+  course_batch = course_temp.each_slice(10).to_a
+  course_batch.each do |batch|
+    course_resp = Typhoeus::Request.new(
+        @base_url + '/api/author/course_templates',
+        method: :post,
+        params: {
+          course_templates: batch
+        },
+        headers: { authorization: @token }
+    )
+    course_resp.on_complete do |response|
+       res = response.code == 201 ? "Course Batch successfully created" : "Course Batch failed with following #{response.code}"
+       puts res
+    end
+    hydra.queue(course_resp)
+  end
+  hydra.run
+end
+
+puts "How many courses would you like to create in Bridge for #{@base_url}?"
+
+courses = gets.chomp
+
+courses(courses.to_i)
